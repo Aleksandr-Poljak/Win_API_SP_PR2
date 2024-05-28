1cef61dbb35cc0593b03f19d7473f58b865dabfd
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-05-28 17:39:53 +0300

    Added processing of WM_LBUTTONDOWN message. Text input by left mouse button click at mouse pointer position.

--- sp_pr2.cpp
+++ sp_pr2.cpp
@@ -72,14 +72,30 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
 LRESULT CALLBACK Pr2_WndProc(HWND hWnd, UINT msg,
 	WPARAM wParam, LPARAM lParam) {
 
+	HDC hdc; // Дескриптор контекста устройства.
+
+	LPCTSTR lpszMessage = TEXT("Обработка сообщения WM_LBUTTONDOWN,\n которое посылается \
+				в окно при щелчке левой кнопки мыши");
+	TCHAR messageFormat[200];
+	wsprintf(messageFormat, lpszMessage);
+
+	int xPos = LOWORD(lParam); // Получаем x-координату
+	int yPos = HIWORD(lParam); // Получаем y-координату
+	RECT rt{ xPos, yPos, xPos + 200, yPos + 50 }; // Создаем прямоугольник для текста;
+
 	switch (msg) {
 		case WM_DESTROY:
-			MessageBox(NULL, g_lpszDestroyMessage, TEXT("Окно закрыто"), MB_OK);
+			//MessageBox(NULL, g_lpszDestroyMessage, TEXT("Окно закрыто"), MB_OK);
 			PostQuitMessage(5);
+			break;
+		// Вывод сообщения при  клике левой кнопкой мыши по координатам мыши
+		case WM_LBUTTONDOWN:	
+			hdc = GetDC(hWnd);
+			DrawText(hdc, messageFormat, lstrlen(messageFormat), &rt, DT_LEFT | DT_TOP | DT_WORDBREAK);
+			ReleaseDC(hWnd, hdc);
 			break;
 		default:
 			return DefWindowProc(hWnd, msg, wParam, lParam);
 	}
 	return false;
-
 }
